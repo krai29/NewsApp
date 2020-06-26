2bfffab039a2e82cc091370ed912c46227d0b7e3
krai29 <karan29rai@gmail.com>
2020-06-27 00:40:20 +0530

    Working on recyclerView adapter

---
+++ app/src/main/java/com/krai29/newsapp/ui/adapter/NewsAdapter.kt
@@ -0,0 +1,53 @@
+package com.krai29.newsapp.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.krai29.newsapp.R
+import com.krai29.newsapp.model.Article
+import kotlinx.android.synthetic.main.item_article_preview.view.*
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+
+    inner class ArticleViewHolder(itemView : View):RecyclerView.ViewHolder(itemView)
+
+    // AsyncListDiffer callback
+    private val differCallback = object : DiffUtil.ItemCallback<Article>(){
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url==newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem==newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this,differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        return ArticleViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_article_preview,parent,false)
+        )
+    }
+
+    override fun getItemCount(): Int = differ.currentList.size
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this).load(article.urlToImage).into(ivArticleImage)
+            tvTitle.text = article.title
+            tvSource.text = article.source.name
+            tvPublishedAt.text = article.publishedAt
+            tvDescription.text = article.description
+
+        }
+    }
+
+
+}
