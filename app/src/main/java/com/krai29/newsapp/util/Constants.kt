5d18f6342c9e6c86444314837de3417cafea75a4
krai29 <karan29rai@gmail.com>
2020-07-04 14:26:37 +0530

    Minor refactoring

--- app/src/main/java/com/krai29/newsapp/util/Constants.kt
+++ app/src/main/java/com/krai29/newsapp/util/Constants.kt
@@ -3,7 +3,7 @@ package com.krai29.newsapp.util
 
 class Constants {
     companion object{
-        const val API_KEY = "56444066e52a4795a5415c444a84dc44"
+        const val API_KEY = "aaaaaaaaa"
         const val NEWS_API_BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
